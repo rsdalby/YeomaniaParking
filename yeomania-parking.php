c8a905d773f89521c875de033c2add9f16f9deee
Steele Dalby <rsdalby@yeomania.org>
2018-12-04 00:10:15 +0000

    Added header

---
+++ yeomania-parking.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: yeomania-parking
+Plugin URI: https://github.com/rsdalby/yeomania-parking
+Description: RV Parking plugin for word press
+Version: 0.0.1
+Author: Steele Dalby
+*/
+
