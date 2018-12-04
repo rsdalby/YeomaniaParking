2aab9fcea1ff25968ccc57eb7f741c525ab3e515
Steele Dalby <rsdalby@yeomania.org>
2018-12-04 21:23:12 +0000

    Added check to prevent direct execution

--- yeomania-parking.php
+++ yeomania-parking.php
@@ -1,9 +1,10 @@
 <?php
 /*
-Plugin Name: yeomania-parking
+Plugin Name: Yeomania RV Parking
 Plugin URI: https://github.com/rsdalby/yeomania-parking
 Description: RV Parking plugin for word press
 Version: 0.0.1
 Author: Steele Dalby
 */
 
+defined( 'ABSPATH' ) or die;
