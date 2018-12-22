bd0fb5f7f45c21150d2eec4aa9299c068a7eafcc
Steele Dalby <rsdalby@yeomania.org>
2018-12-22 20:24:54 +0000

    Added flush rewrite rules to activation/deactivation.

--- YeomaniaParking.php
+++ YeomaniaParking.php
@@ -12,11 +12,11 @@ defined( 'ABSPATH' ) or die( "Script must be executed within wordpress.\n" );
 class YeomaniaParking{
 
   function activate() {
-
+    flush_rewrite_rules();
   }  
 
   function deactivate() {
-
+    flush_rewrite_rules();
   }
 
   function uninstall() {
@@ -29,3 +29,4 @@ if ( class_exists( 'YeomaniaParking' ) ) {
 }
 
 register_activation_hook( __FILE__, array( $YeomaniaParking, 'activate' ) );
+register_deactivation_hook( __FILE__, array( $YeomaniaParking, 'deactivate' ) );
